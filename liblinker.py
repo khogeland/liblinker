d9c8f6b7b7f6e5d456c71b4de61d5653c8363ea2
Kevin Hogeland <kevin@hoge.land>
2015-08-18 23:42:39 -0700

    v1.0.0 release
    creates links and installs as a CLI utility

--- liblinker.py
+++ liblinker.py
@@ -1,20 +1,23 @@
+#! python3
 import mutagen
 import struct
 from mutagen import mp3, flac, mp4
-from tinytag import TinyTag as tt
+from tinytag import TinyTag
 import os
+import re
 import sys
 
-if len(sys.argv) < 3:
-    exit('usage: liblinker in_directory out_directory')
-
+silent = True if len(sys.argv) == 4 and sys.argv[3] == '--silent' else False
 formats = ['mp3', 'flac', 'wav', 'wave', 'm4a', 'aac']
-in_dir, out_dir = sys.argv[1], sys.argv[2]
+in_dir, out_dir = os.path.normpath(sys.argv[1]), os.path.normpath(sys.argv[2])
 bad_files = False
-tags = {}
+all_tags = {}
+slash = str(os.path.sep)
 
 
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    if silent:
+        return
     enc = file.encoding
     if enc == 'UTF-8':
         print(*objects, sep=sep, end=end, file=file)
@@ -26,11 +29,11 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
 def walk(name):
     if os.path.isdir(name):
         for f in os.listdir(name):
-            walk('%s/%s' % (name, f))
+            walk(name + slash + f)
     else:
         ext = os.path.splitext(name)[1].lower()[1:]
         if ext in formats:
-            tags[name] = get_album_artist(name, ext)
+            all_tags[name] = get_album_artist(name, ext)
 
 
 def get_album_artist(name, ext, infer=True):
@@ -44,19 +47,20 @@ def get_album_artist(name, ext, infer=True):
     }[ext](name, infer)
 
 
-def infer_album_artist(name, tag_func):
+def infer_album_artist(name):
     parent = os.path.abspath(os.path.join(name, os.pardir))
     raw_tags = []
     for f in os.listdir(parent):
-        full_name = '%s/%s' % (parent, f)
+        full_name = parent + slash + f
         ext = f.split('.')[-1].lower()
         if os.path.isfile(full_name) and ext in formats:
             raw_tags.append(get_album_artist(full_name, ext, False))
-    tags = [ t for t in raw_tags if t is not None and t[0] is not None and t[1] is not None]
+    tags = [t for t in raw_tags if t is not None and t[0] is not None and t[1] is not None]
     artists = list(set(map(lambda x: x[0], tags)))
     albums = list(set(map(lambda x: x[1], tags)))
     i_tags = artists[0] if len(artists) == 1 else None, albums[0] if len(albums) == 1 else None
-    if not any(i_tags):
+    if not any(i_tags) or (i_tags[0] is None and i_tags[1] is not None):
+        i_tags = None
         global bad_files
         bad_files = True
     uprint(i_tags)
@@ -68,13 +72,14 @@ def tags_mp3(name, infer=True):
         tags = mp3.MP3(name).tags
     except mp3.HeaderNotFoundError:
         if infer:
-            uprint('WARN: File appears to be corrupt or malformed, attempting to infer artist and album: %s ...' % name, end=' ')
-            return infer_album_artist(name, tags_mp3)
+            uprint('WARN: Corrupted or mistagged, inferring artist and album: %s ...' % name, end=' ')
+            return infer_album_artist(name)
         return None
     if not tags:
         return None, None
-    return tags['TPE1'].text[0] if 'TPE1' in tags.keys() else tags['TOPE'] if 'TOPE' in tags.keys() else None,\
-           tags['TALB'].text[0] if 'TALB' in tags.keys() else tags['TOAL'] if 'TOAL' in tags.keys() else None
+    return \
+        tags['TPE1'].text[0] if 'TPE1' in tags.keys() else tags['TOPE'] if 'TOPE' in tags.keys() else None,\
+        tags['TALB'].text[0] if 'TALB' in tags.keys() else tags['TOAL'] if 'TOAL' in tags.keys() else None
 
 
 def tags_flac(name, infer=True):
@@ -82,41 +87,80 @@ def tags_flac(name, infer=True):
         tags = flac.FLAC(name)
     except mutagen.flac.FLACNoHeaderError:
         if infer:
-            uprint('WARN: File appears to be corrupt or malformed, attempting to infer  artist and album: %s ...' % name, end=' ')
-            return infer_album_artist(name, tags_flac)
+            uprint('WARN: Corrupted or mistagged, inferring artist and album: %s ...' % name, end=' ')
+            return infer_album_artist(name)
         return None
     if not tags:
         return None, None
-    return tags['album'][0], tags['albumartist'][0] if 'albumartist' in tags.keys() else tags['artist'][0]
+    return tags['albumartist'][0] if 'albumartist' in tags.keys() else tags['artist'][0], tags['album'][0]
 
 
 def tags_wav(name, infer=True):
     try:
-        tags = tt.get(name)
+        tags = TinyTag.get(name)
     except struct.error:
         if infer:
-            uprint('WARN: File appears to be corrupt or malformed, attempting to infer  artist and album: %s ...' % name, end=' ')
-            return infer_album_artist(name, tags_wav)
+            uprint('WARN: Corrupted or mistagged, inferring artist and album: %s ...' % name, end=' ')
+            return infer_album_artist(name)
         return None
     if not tags:
         return None, None
     return tags.artist, tags.album
+
 
 def tags_mp4(name, infer=True):
     try:
         tags = mp4.MP4(name)
     except mp4.MP4StreamInfoError:
         if infer:
-            uprint('WARN: File appears to be corrupt or malformed, attempting to infer  artist and album: %s ...' % name, end=' ')
-            return infer_album_artist(name, tags_mp4)
+            uprint('WARN: Corrupted or mistagged, inferring artist and album: %s ...' % name, end=' ')
+            return infer_album_artist(name)
         return None
     if not tags:
         return None, None
-    return tags['\xa9alb'][0] if '\xa9alb' in tags.keys() else None,\
-           tags['\xa9ART'][0] if '\xa9ART' in tags.keys() else None
+    return \
+        tags['\xa9ART'][0] if '\xa9ART' in tags.keys() else None,\
+        tags['\xa9alb'][0] if '\xa9alb' in tags.keys() else None
 
-walk(in_dir)
-uprint(len(tags))
-if bad_files:
-    uprint('WARN: There are corrupted or mistagged files in your library that could not be organized. '
-           'They have been put in "%s/ErrorFiles" so you may organize them manually.' % out_dir)
+
+def make_links():
+    for path in all_tags:
+        if not all_tags[path] or not all_tags[path][0]:
+            new_path = out_dir + slash + '_ErrorFiles' + slash + os.path.split(path.replace(in_dir, ''))[0]
+        else:
+            artist, album = all_tags[path]
+            artist = clean(artist)
+            album = clean(album) if album else 'Unknown Album'
+            new_path = out_dir + slash + artist + slash + album
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+        tail = os.path.split(path)[1]
+        new_path += slash + clean(tail)
+        if not os.path.exists(new_path):
+            try:
+                os.link(path, new_path)
+            except OSError:
+                os.symlink(path, new_path)
+
+
+def clean(string):
+    return re.sub(r'\s+$', '', re.sub(r'[<>:"/\\\|\?\*]', '', string))
+
+
+def main():
+    try:
+        if len(sys.argv) < 3:
+            exit('Usage: liblinker IN_DIR OUT_DIR [--silent]')
+        uprint('INFO: Scanning music library at "%s"' % in_dir)
+        walk(in_dir)
+        uprint('INFO: Creating links at "%s"' % out_dir)
+        make_links()
+        if bad_files:
+                uprint('WARN: There are corrupted or mistagged files in your library that could not be organized. '
+                       'They have been put in "%s%s_ErrorFiles" so you may organize them manually.' % (out_dir, slash))
+    except KeyboardInterrupt:
+        exit('Interrupted.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='liblinker',
+      version='1.0.0',
+      description='Organize your music library without altering the original directory.',
+      url='https://github.com/btym/liblinker',
+      author='Kevin Hogeland',
+      author_email='kevin@hoge.land',
+      license='MIT',
+      py_modules=['liblinker'],
+      entry_points={
+          'console_scripts': ['liblinker=liblinker:main']
+      },
+      zip_safe=True,
+      install_requires=[
+          'mutagen',
+          'tinytag'
+      ])
